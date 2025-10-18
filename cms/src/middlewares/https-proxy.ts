287db4dddd7984ae7dfdc421b471df4094dfa119
algorhythmicdev <u8068068961@gmail.com>
2025-10-18 18:32:49 +0300

    v

--- cms/src/middlewares/https-proxy.ts
+++ cms/src/middlewares/https-proxy.ts
@@ -3,7 +3,10 @@ export default (_config, { strapi }) => {
     // If Cloud Run/LB says the original scheme was HTTPS, mark the request secure so Koa can issue secure cookies.
     const xfproto = ctx.headers['x-forwarded-proto'];
     if (xfproto && xfproto.toString().toLowerCase() === 'https') {
-      ctx.request.secure = true;
+      ctx.request.header['x-forwarded-proto'] = 'https';
+      if (ctx.request.socket) {
+        ctx.request.socket.encrypted = true;
+      }
     }
     await next();
   };
